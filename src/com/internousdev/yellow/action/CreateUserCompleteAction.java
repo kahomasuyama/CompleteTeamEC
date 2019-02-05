e911f0d33e54958288290161b90a5f0e7aff5759
KazumaSakai <mail@shidenvip.jp>
2019-02-05 16:39:31 +0900

    [fix] CreateUserCompleteAction

--- src/com/internousdev/yellow/action/CreateUserCompleteAction.java
+++ src/com/internousdev/yellow/action/CreateUserCompleteAction.java
@@ -2,10 +2,13 @@ package com.internousdev.yellow.action;
 
 import java.util.Map;
 
+import org.apache.struts2.interceptor.SessionAware;
+
 import com.internousdev.yellow.dao.UserInfoDAO;
+import com.opensymphony.xwork2.ActionSupport;
 
 public class CreateUserCompleteAction extends ActionSupport implements SessionAware{
-	
+
 	private String familyName;
 	private String firstName;
 	private String familyNameKana;
@@ -15,88 +18,96 @@ public class CreateUserCompleteAction extends ActionSupport implements SessionAw
 	private String loginId;
 	private String password;
 	private String categoryId;
+
 	private Map<String,Object> session;
-	private String execute() {
-		String result = ERROR;
+
+	public String execute()
+	{
+		//	ユーザーを作成
 		UserInfoDAO UserInfoDao = new UserInfoDAO();
 		int count = UserInfoDao.createUser(familyName, firstName, familyNameKana, firstNameKana, sex, email, loginId, password);
-		if(count > 0) {
-			result = SUCCESS;
+
+		if(count > 0)
+		{
+			return SUCCESS;
 		}
-		return result;
+		else
+		{
+			return ERROR;
+		}
 	}
-	
+
 	public String getCategoryId() {
 		return categoryId;
 	}
 	public void setCategoryId(String categoryId) {
 		this.categoryId = categoryId;
 	}
-	
+
 	public String getFamilyName() {
 		return familyName;
 	}
 	public void setFamilyName(String familyName) {
 		this.familyName = familyName;
 	}
-	
+
 	public String getFirstName() {
 		return firstName;
 	}
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-	
+
 	public String getFamilyNameKana() {
 		return familyNameKana;
 	}
 	public void setFamilyNameKana(String familyNameKana) {
 		this.familyNameKana = familyNameKana;
 	}
-	
+
 	public String getFirstNameKana() {
 		return firstNameKana;
 	}
 	public void setFirstNameKana(String firstNameKana) {
 		this.firstNameKana = firstNameKana;
 	}
-	
+
 	public String getSex() {
 		return sex;
 	}
 	public void setSex(String sex) {
 		this.sex = sex;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public String getLoginId() {
 		return loginId;
 	}
 	public void setLoginId(String loginId) {
 		this.loginId = loginId;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public Map<String, Object> getSession() {
 		return session;
 	}
 	public void setSession(Map<String, Object> session) {
 		this.session = session;
 	}
-	
-	
+
+
 
 
 
