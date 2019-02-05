101e298a2b02eff108e7fbd32800af5940e46f50
KazumaSakai <mail@shidenvip.jp>
2019-02-05 15:17:03 +0900

    [fix] ProductDetailsActionを修正

--- src/com/internousdev/yellow/action/ProductDetailsAction.java
+++ src/com/internousdev/yellow/action/ProductDetailsAction.java
@@ -1,58 +1,28 @@
 package com.internousdev.yellow.action;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
 import com.internousdev.yellow.dao.ProductInfoDAO;
-import com.internousdev.yellow.dto.MCategoryDTO;
 import com.internousdev.yellow.dto.ProductInfoDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class ProductDetailsAction extends ActionSupport implements SessionAware {
+
 	private int productId;
-	private List<MCategoryDTO> mCategoryDtoList = new ArrayList<MCategoryDTO>();
-	private ProductInfoDTO productInfoDTO = new ProductInfoDTO();
+	private ProductInfoDTO productInfoDTO;
 	private String categoryId;
 	private Map<String, Object> session;
+
 	public String execute() {
 		String result = ERROR;
 		ProductInfoDAO productInfoDAO = new ProductInfoDAO();
-		ProductInfoDTO productInfoDTO = new ProductInfoDTO();
-		productInfoDTO = productInfoDAO.getProductInfo(productId);
-		session.put("id", productInfoDTO.getId());
-		session.put("productId", productInfoDTO.getProductId());
-		session.put("productName", productInfoDTO.getProductName());
-		session.put("productNameKana", productInfoDTO.getProductNameKana());
-		session.put("imageFilePath", productInfoDTO.getImageFilePath());
-		session.put("iamgeFileName", productInfoDTO.getImageFileName());
-		session.put("price", productInfoDTO.getPrice());
 
-		session.put("releaseCompany", productInfoDTO.getReleaseCompany());
-		session.put("releaseDate", productInfoDTO.getReleaseDate());
-		session.put("productDescription", productInfoDTO.getProductDescription());
-		List<Integer> productCountList = new ArrayList<Integer> (Arrays.asList(1,2,3,4,5));
-		session.put("productCountList", productCountList);
-		productInfoDTO = productInfoDAO.getProductInfo(productInfoDTO.getCategoryId(), productInfoDTO.getProductId(), 0, 3);
-		Iterator<ProductInfoDTO> iterator = productInfoDTO.iterator();
-		if(!(iterator.hasNext())) {
-			productCountList = null;
-		}
+		productInfoDTO = productInfoDAO.getProductInfo(productId);
 
 		return result;
 	}
-	public List<MCategoryDTO> getmCategoryDtoList() {
-		return mCategoryDtoList;
-	}
-
-	public void setmCategoryDtoList(List<MCategoryDTO> mCategoryDtoList) {
-		this.mCategoryDtoList = mCategoryDtoList;
-	}
-
 	public String getCategoryId() {
 		return categoryId;
 	}
