78e182b0e08a50860169ddb8f592eef3dda9a104
KazumaSakai <mail@shidenvip.jp>
2019-02-05 13:43:39 +0900

    [add] HomeActionを追加

---
+++ src/com/internousdev/yellow/action/HomeAction.java
@@ -0,0 +1,36 @@
+package com.internousdev.yellow.action;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.internousdev.yellow.dao.MCategoryDAO;
+import com.internousdev.yellow.dto.MCategoryDTO;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HomeAction extends ActionSupport implements SessionAware
+{
+	private Map<String, Object> session;
+
+	public String execute()
+	{
+		if(!session.containsKey("mCategoryDtoList"))
+		{
+			MCategoryDAO mcategoryDAO = new MCategoryDAO();
+			List<MCategoryDTO> mCategoryDtoList = mcategoryDAO.getMCategoryList();
+			session.put("mCategoryDtoList", mCategoryDtoList);
+		}
+
+		return SUCCESS;
+	}
+
+	public Map<String, Object> getSession() {
+		return session;
+	}
+
+	@Override
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+}
