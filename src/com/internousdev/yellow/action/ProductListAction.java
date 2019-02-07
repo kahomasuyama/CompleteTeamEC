8d123d685309fc9f19fd0daec89f9fb5a26e0622
KazumaSakai <mail@shidenvip.jp>
2019-02-07 12:26:25 +0900

    [fix] ProductListActionを修正

--- src/com/internousdev/yellow/action/ProductListAction.java
+++ src/com/internousdev/yellow/action/ProductListAction.java
@@ -14,9 +14,6 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class ProductListAction extends ActionSupport implements SessionAware
 {
-	//	Receive
-	private int productId;
-
 	//	Send
 	private List<MCategoryDTO> mCategoryDtoList = new ArrayList<MCategoryDTO>();
 	private List<ProductInfoDTO> productInfoList = new ArrayList<ProductInfoDTO>();
@@ -47,12 +44,6 @@ public class ProductListAction extends ActionSupport implements SessionAware
 	}
 	public void setmCategoryDtoList(List<MCategoryDTO> mCategoryDtoList) {
 		this.mCategoryDtoList = mCategoryDtoList;
-	}
-	public int getProductId() {
-		return productId;
-	}
-	public void setProductId(int productId) {
-		this.productId = productId;
 	}
 	public List<ProductInfoDTO> getProductInfoList() {
 		return productInfoList;
