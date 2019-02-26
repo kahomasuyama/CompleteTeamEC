ce7c4e3949a1e8ffe5c6e6b1891c60f8677e52f0
gennarikun <gennarikun@gmail.com>
2019-02-26 11:16:58 +0900

    [fix]CreateDestinationCompleteAction修正。

--- src/com/internousdev/yellow/action/CreateDestinationCompleteAction.java
+++ src/com/internousdev/yellow/action/CreateDestinationCompleteAction.java
@@ -1,10 +1,13 @@
 package com.internousdev.yellow.action;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
 import com.internousdev.yellow.dao.DestinationInfoDAO;
+import com.internousdev.yellow.util.InputChecker;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class CreateDestinationCompleteAction extends ActionSupport implements SessionAware
@@ -17,6 +20,14 @@ public class CreateDestinationCompleteAction extends ActionSupport implements Se
 	private String tellNumber;
 	private String userAddress;
 	private Map<String, Object> session;
+	private List<String> errorMsgList;
+	private List<String> familyNameMsgList;
+	private List<String> firstNameMsgList;
+	private List<String> familyNameKanaMsgList;
+	private List<String> firstNameKanaMsgList;
+	private List<String> emailMsgList;
+	private List<String> tellNumberMsgList;
+	private List<String> userAddressMsgList;
 
 	public String execute()
 	{
@@ -27,6 +38,32 @@ public class CreateDestinationCompleteAction extends ActionSupport implements Se
 		}
 
 		DestinationInfoDAO destinationInfoDao = new DestinationInfoDAO();
+
+		InputChecker inputChecker = new InputChecker();
+
+		errorMsgList = new ArrayList<String>();
+
+		familyNameMsgList = inputChecker.doCheck("姓",familyName,1,16,true,true, true, false, false, false, false, false, false);
+		firstNameMsgList = inputChecker.doCheck("名", firstName, 1, 16, true, true, true, false, false, true, false, false, false);
+		familyNameKanaMsgList = inputChecker.doCheck("姓ふりがな", familyNameKana, 1, 16, false, false, true, false, false, false, false, false, false);
+		firstNameKanaMsgList = inputChecker.doCheck("名ふりがな",  firstNameKana, 1, 16, false, false, true, false, false, false, false, false, false);
+		emailMsgList = inputChecker.doCheck("メールアドレス", email, 10, 32, true, false, false, true, true, false, false, false, false);
+		tellNumberMsgList = inputChecker.doCheck("電話番号", tellNumber, 10, 13, false, false, false, true, false, false, false, false, false);
+		userAddressMsgList = inputChecker.doCheck("住所", userAddress, 10, 50, true, true, true, true, false, true, false, false, false);
+
+        //	入力値チェックで、エラーがなかったら
+        if(!familyNameMsgList.isEmpty()
+        	||!firstNameMsgList.isEmpty()
+        	||!familyNameKanaMsgList.isEmpty()
+        	||!firstNameKanaMsgList.isEmpty()
+        	||!emailMsgList.isEmpty()
+        	||!tellNumberMsgList.isEmpty()
+			||!userAddressMsgList.isEmpty())
+
+			{
+        	return ERROR;
+        }
+
 		int count = destinationInfoDao.insert(String.valueOf(session.get("loginId")), familyName, firstName, familyNameKana, firstNameKana, email, tellNumber, userAddress);
 		if(count > 0)
 		{
@@ -100,5 +137,11 @@ public class CreateDestinationCompleteAction extends ActionSupport implements Se
 
 	public void setSession(Map<String, Object> session) {
 		this.session = session;
+	}
+	public List<String> getErrorMsgList() {
+		return errorMsgList;
+	}
+	public void setErrorMsgList(List<String> errorMsgList) {
+		this.errorMsgList = errorMsgList;
 	}
 }
